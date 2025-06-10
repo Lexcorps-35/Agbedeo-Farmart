3604cbacc565433d66db017c6716d16d26ee577a
Ene Alexander <ene875688@gmail.com>
2025-06-10 20:34:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,20 @@ import { About } from './components/About'
 import Products from './components/Products'
 import Footer from './components/Footer'
 import Comments from './components/comments'
-
+import Home from './components/Home'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Home />
       < Hotdeals/>
       <About />
       <Products />
       <Comments />
-      < Footer/>
-      </div>
-  )
+      < Footer />
+    </div>
+  );
 }
 
 export default App
