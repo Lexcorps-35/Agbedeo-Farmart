dee1bc62520d54d3e41969c8c8694e08d55c1775
Ene Alexander <ene875688@gmail.com>
2025-06-10 20:35:16 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
 
   return (
     <div>
-        <nav className='flex justify-between items-center
+        <nav className='flex justify-between items-center w-full
         text-black py-4 px-8 md:px-32 bg-blue-800/50 drop-shadow-md' data-aos="fade-down">
        <a href="#" className='hover:scale-105 transition-all text-amber-700 font-extrabold font-serif text-3xl md:text-4xl'>Agboedo</a>
        <ul className='hidden md:flex items-center gap-12
@@ -29,10 +29,10 @@ const Navbar = () => {
         hover:text-red-800 rounded-md  cursor-pointer'>Product</li></a>
         <a href="#contact"><li className='p-1 hover:bg-blue-500
         hover:text-red-800 rounded-md cursor-pointer'>Contact</li></a>
-       </ul>
+              </ul>
        <div className='relative hidden md:flex items-center 
        justify-center gap-3'>
-       < IoSearchSharp className='absolute left-3 text-2xl text-shadow-gray-500'/>
+       < IoSearchSharp className=' absolute left-3 text-2xl text-shadow-gray-500'/>
          <input type="text" placeholder='Search...' className='py-1 pl-10 rounded-xl 
          border border-gray-300 focus:outline-none focus:border-2 focus:border-blue-600 transition-all duration-300
          '/>
@@ -45,29 +45,7 @@ const Navbar = () => {
       
   
 
-        <section id='home'>
-    <div className='' data-aos="fade-left">
-        <img 
-            src="https://media.istockphoto.com/id/1398965606/photo/father-and-daughter-shopping-in-a-grocery-store.jpg?s=612x612&w=0&k=20&c=irfHWhH_DeaUp7fopzromq39mH8-QFYrnV04UbXr6NU="
-            alt="header" 
-            className='float-right h-[30rem] w-full md:w-[60rem] object-cover'
-        />
-    </div>
-    <div>
-        <div 
-            className='absolute inset-0 rounded-2xl p-4  h-auto md:h-[28rem] w-full md:w-[33rem] bg-white/50 backdrop-blur-sm mt-[5rem]  mx-4 md:mx-0' 
-            data-aos="fade-right" 
-            data-aos-delay="300"
-        >
-            <div className='' data-aos="fade-up">
-                <h1 className='text-3xl md:text-6xl font-extrabold font-serif'>Agboedo: Your One-Stop Grocery Shop</h1>
-                <p className='mt-4 text-gray-700 font-bold text-base md:text-2xl'>
-                    Enjoy a convenient and pleasant shopping experience with high-quality products and exceptional customer service, all tailored to meet your everyday grocery needs.
-                </p>
-            </div>
-        </div>
-    </div>
-</section>
+        
     </div>
     
   )
